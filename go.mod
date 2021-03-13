fa2d077926b8fe29093ab6340256081981582e2b
eru <eru160@protonmail.com>
2021-03-13 13:52:32 +0900

    TUI

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/eru1a/vgate
 
 go 1.16
 
-require github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4
+require (
+	github.com/gdamore/tcell/v2 v2.2.0
+	github.com/gocarina/gocsv v0.0.0-20201208093247-67c824bc04d4
+	github.com/rivo/tview v0.0.0-20210312174852-ae9464cc3598
+)
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
-	"os/signal"
-	"text/tabwriter"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/gocarina/gocsv"
+	"github.com/rivo/tview"
 )
 
 var (
@@ -35,6 +35,139 @@ type Server struct {
 	OpenVPNConfigDataBase64 string `csv:"OpenVPN_ConfigData_Base64"`
 }
 
+type App struct {
+	*tview.Application
+	servers     []Server
+	flex        *tview.Flex
+	serversView *ServersView
+	outputView  *tview.TextView
+	cmd         *exec.Cmd
+}
+
+func NewApp(servers []Server) *App {
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow)
+	serversView := NewServersView(servers)
+	outputView := tview.NewTextView()
+	outputView.SetTitle("output").SetTitleAlign(tview.AlignLeft).SetBorder(true)
+	flex.AddItem(serversView, 0, 1, true).
+		AddItem(outputView, 0, 1, false)
+
+	app := tview.NewApplication()
+	app.SetRoot(flex, true)
+
+	return &App{
+		Application: app,
+		servers:     servers,
+		flex:        flex,
+		serversView: serversView,
+		outputView:  outputView,
+	}
+}
+
+func (a *App) setAction() {
+	a.serversView.SetSelectedFunc(func(row, column int) {
+		if row > len(a.servers) {
+			return
+		}
+		if a.cmd != nil {
+			a.cmd.Process.Kill()
+			a.cmd = nil
+			a.outputView.Clear()
+		}
+
+		server := a.servers[row-1]
+
+		cmd, err := ConnectCmd(server.OpenVPNConfigDataBase64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		a.cmd = cmd
+		a.cmd.Stdout = a.outputView
+		a.cmd.Stderr = a.outputView
+
+		go func() {
+			a.cmd.Run()
+		}()
+	})
+
+	a.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyCtrlC:
+			a.Stop()
+			return nil
+		}
+		switch event.Rune() {
+		case 'q':
+			a.Stop()
+			return nil
+		}
+		return event
+	})
+
+	a.outputView.SetChangedFunc(func() {
+		a.Draw()
+	})
+}
+
+func (a *App) Run() {
+	defer os.Remove("/tmp/openvpnconf")
+
+	a.setAction()
+
+	if err := a.Application.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (a *App) Stop() {
+	if a.cmd != nil {
+		a.cmd.Process.Kill()
+		a.cmd = nil
+	}
+	a.Application.Stop()
+}
+
+type ServersView struct {
+	*tview.Table
+	Servers []Server
+}
+
+func NewServersView(servers []Server) *ServersView {
+	table := tview.NewTable()
+
+	table.SetCell(0, 0, tview.NewTableCell("Country"))
+	table.SetCell(0, 1, tview.NewTableCell("IP"))
+	table.SetCell(0, 2, tview.NewTableCell("Ping"))
+	table.SetCell(0, 3, tview.NewTableCell("Speed"))
+	table.SetCell(0, 4, tview.NewTableCell("Score"))
+	table.SetCell(0, 5, tview.NewTableCell("TotalUsers"))
+	table.SetCell(0, 6, tview.NewTableCell("TotalTraffic"))
+
+	for i, server := range servers {
+		table.SetCell(i+1, 0, tview.NewTableCell(server.CountryShort))
+		table.SetCell(i+1, 1, tview.NewTableCell(server.IP))
+		table.SetCell(i+1, 2, tview.NewTableCell(server.Ping))
+		table.SetCell(i+1, 3, tview.NewTableCell(server.Speed))
+		table.SetCell(i+1, 4, tview.NewTableCell(server.Score))
+		table.SetCell(i+1, 5, tview.NewTableCell(server.TotalUsers))
+		table.SetCell(i+1, 6, tview.NewTableCell(server.TotalTraffic))
+	}
+
+	table.Select(1, 0).SetFixed(1, 1).SetSelectable(true, false)
+	table.SetSelectionChangedFunc(func(row, column int) {
+		if row == 0 {
+			table.Select(1, 0)
+		}
+	})
+	table.SetTitle("servers").SetTitleAlign(tview.AlignLeft).SetBorder(true)
+
+	return &ServersView{
+		Table:   table,
+		Servers: servers,
+	}
+}
+
 func main() {
 	country := "JP"
 
@@ -50,33 +183,8 @@ func main() {
 		}
 	}
 
-	server := choseServer(servers)
-
-	if err := Connect(server.OpenVPNConfigDataBase64); err != nil {
-		panic(err)
-	}
-}
-
-func choseServer(servers []Server) Server {
-	var index int
-	for {
-		w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-		w.Write([]byte("Index\tCountry\tIP\tPing\tSpeed\tScore\n"))
-		for i, server := range servers {
-			w.Write([]byte(fmt.Sprintf("%d\t%s\t%s\t%s\t%s\t%s\n", i,
-				server.CountryShort, server.IP, server.Ping, server.Speed, server.Score)))
-		}
-		w.Flush()
-		fmt.Printf("choose index: ")
-		_, err := fmt.Scanln(&index)
-		if err != nil {
-			continue
-		}
-		if index >= 0 && index < len(servers) {
-			break
-		}
-	}
-	return servers[index]
+	app := NewApp(servers)
+	app.Run()
 }
 
 func getServers() ([]Server, error) {
@@ -107,32 +215,25 @@ func getServers() ([]Server, error) {
 	return servers, nil
 }
 
-func Connect(openVPNConfigDataBase64 string) error {
+func ConnectCmd(openVPNConfigDataBase64 string) (*exec.Cmd, error) {
 	conf, err := base64.StdEncoding.DecodeString(openVPNConfigDataBase64)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	f, err := os.Create("/tmp/openvpnconf")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
-	defer os.Remove("/tmp/openvpnconf")
 
 	_, err = f.Write(conf)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
-	cmd.Stdout = os.Stdout
+	// cmd := exec.Command("sudo", "openvpn", "/tmp/openvpnconf")
+	cmd := exec.Command("openvpn", "/tmp/openvpnconf")
 
-	signal.Ignore(os.Interrupt)
-
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-
-	return nil
+	return cmd, nil
 }
